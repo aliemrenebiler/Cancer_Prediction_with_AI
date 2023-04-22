590a1be8893d42210efcf16607e2d9a1ffe823b5
Ali Emre Nebiler <emre_nebiler@windowslive.com>
2023-04-22 15:28:34 +0300

    changed output names and moved old files to folder

---
+++ dataset_preprocesses/2_remove_batch_effect_v3.R
@@ -0,0 +1,96 @@
+library(ggfortify)
+library(limma)
+library(svglite)
+
+# Set working directory
+setwd("~/Desktop")
+
+# ------------------------------------------------------------------------------
+# CONFIGURATIONS
+# ------------------------------------------------------------------------------
+
+# Set file names
+mdata_file_name <- "1_merged_mdata.csv"
+exprs_file_name <- "1_merged_exprs.csv"
+corrected_exprs_file_name <- "2_corrected_exprs.csv"
+pca_before_file_name <- "2_pca_before.svg"
+pca_after_file_name <- "2_pca_after.svg"
+
+# ------------------------------------------------------------------------------
+# GET TABLES FROM CSV FILE
+# ------------------------------------------------------------------------------
+
+message("Reading CSV files...")
+
+# Read in sample expression data
+exprs_table <- read.csv(exprs_file_name, row.names = 1)
+
+# Read in sample metadata
+mdata_table <- read.csv(mdata_file_name, row.names = 1)
+
+# Making sure the sample names in expressions matches
+# to sample names in metadata in the same order
+if(
+  all(rownames(exprs_table) %in% rownames(mdata_table))
+  && all(rownames(exprs_table) == rownames(mdata_table))
+) {
+  message("Sample names are matching.")
+} else {
+  stop("Sample names are not matching.")
+}
+
+# ------------------------------------------------------------------------------
+# REMOVE BATCH EFFECT AND NORMALIZE
+# ------------------------------------------------------------------------------
+
+message('Removing batch effect...')
+
+# Reverse column and row names, create matrix 
+exprs_table_matrix <- as.matrix.data.frame(t(exprs_table))
+mode(exprs_table_matrix)='numeric'
+
+# Remove batch effect
+exprs_table_corrected <- removeBatchEffect(exprs_table_matrix, mdata_table[,"Dataset"])
+
+# Turn matrix into data frame
+exprs_table_corrected <- as.data.frame(t(exprs_table_corrected))
+colnames(exprs_table_corrected) <- colnames(exprs_table)
+rownames(exprs_table_corrected) <- rownames(exprs_table)
+
+# ------------------------------------------------------------------------------
+# CREATE BEFORE AND AFTER PLOTS
+# ------------------------------------------------------------------------------
+
+message('Creating "before" plot...')
+
+# Create before PCA
+pca_before <- prcomp(exprs_table, scale. = TRUE)
+exprs_table["Dataset"] <- mdata_table[,"Dataset"]
+autoplot(pca_before, data=exprs_table, colour = 'Dataset')
+ggsave(pca_before_file_name)
+exprs_table <- exprs_table[ !colnames(exprs_table) %in% c('Dataset') ]
+
+message('Creating "after" plot...')
+
+# Create after PCA
+pca_after <- prcomp(exprs_table_corrected, scale. = TRUE)
+exprs_table_corrected["Dataset"] <- mdata_table[,"Dataset"]
+autoplot(pca_after, data=exprs_table_corrected, colour = 'Dataset')
+ggsave(pca_after_file_name)
+exprs_table_corrected <- exprs_table_corrected[ !colnames(exprs_table_corrected) %in% c('Dataset')]
+
+# Close the plots
+if (length(dev.list()) > 0) {
+  dev.off()
+}
+
+# ------------------------------------------------------------------------------
+# SAVE AS CSV
+# ------------------------------------------------------------------------------
+
+message("Saving as CSV...")
+write.csv(exprs_table_corrected, file = corrected_exprs_file_name)
+
+# ------------------------------------------------------------------------------
+
+message("Completed.")
---
+++ dataset_preprocesses/3_differencial_gene_expression_v4.R
@@ -0,0 +1,140 @@
+library(Biobase)
+library(limma)
+library(genefilter)
+
+# Set working directory
+setwd("~/Desktop")
+
+# ------------------------------------------------------------------------------
+# CONFIGURATIONS
+# ------------------------------------------------------------------------------
+
+# Set file names
+mdata_file_name <- "1_merged_mdata.csv"
+exprs_file_name <- "2_corrected_exprs.csv"
+deg_genes_file_name <- "3_deg_genes.csv"
+deg_exprs_file_name <- "3_deg_exprs.csv"
+up_genes_file_name <- "3_up_genes.csv"
+up_table_file_name <- "3_up_table.csv"
+down_genes_file_name <- "3_down_genes.csv"
+down_table_file_name <- "3_down_table.csv"
+
+# ------------------------------------------------------------------------------
+# PREPARING COUNT DATA
+# ------------------------------------------------------------------------------
+
+message("Reading CSV files...")
+
+# Read in sample expression data
+exprs_table <- read.csv(exprs_file_name, row.names = 1)
+
+# Read in sample metadata
+mdata_table <- read.csv(mdata_file_name, row.names = 1)
+
+# Making sure the sample names in expressions matches
+# to sample names in metadata in the same order
+if(
+  all(rownames(exprs_table) %in% rownames(mdata_table))
+  && all(rownames(exprs_table) == rownames(mdata_table))
+) {
+  message("Sample names are matching.")
+} else {
+  stop("Sample names are not matching.")
+}
+
+# ------------------------------------------------------------------------------
+# DIFFERENTIAL GENE EXPRESSION ANALYSIS
+# ------------------------------------------------------------------------------
+
+message("DGE analysis in progress...")
+
+# Convert rows into columns and columns into rows as matrix
+exprs_mat <- as.matrix(t(exprs_table))
+
+# Get MSS statuses
+pdata <- as.data.frame(mdata_table$Status)
+rownames(pdata) <- rownames(mdata_table)
+colnames(pdata) = "Label"
+
+# Change labels as MSI or MSS
+table(pdata)
+pdata[pdata == "MSI-H"] <- "MSI"
+pdata[pdata == "MSI-L"] <- "MSS"
+table(pdata)
+
+# Create expression set
+exprs_set <- ExpressionSet(assayData = exprs_mat, phenoData = AnnotatedDataFrame(pdata))
+table(pData(exprs_set)[, "Label"])
+
+# Create the design matrix
+design <- model.matrix(~0 + Label, data = pData(exprs_set))                      
+head(design, 4)
+colSums(design)
+
+# Create the contrast matrix
+cm <- makeContrasts(
+  MSIvMSS = LabelMSI - LabelMSS,
+  levels = design
+)
+
+# Fit coefficients
+fit <- lmFit(exprs_set, design)
+
+# Fit contrasts
+fit2 <- contrasts.fit(fit, contrasts = cm)
+
+# Calculate t-statistics
+fit2 <- eBayes(fit2)
+
+# Summarize results
+results <- decideTests(fit2) # Default: p.value = 0.05, lfc = 0 
+summary(results)
+
+# ------------------------------------------------------------------------------
+# SAVE AS CSV
+# ------------------------------------------------------------------------------
+
+message("Saving as CSV...")
+
+up <- which(results[,1] == 1)
+upGenes <- fit2[up, ]
+upTable <- topTable(upGenes[,1], number = nrow(fit2), sort.by = "logFC") 
+upTable$gene <- rownames(upTable)
+upTable <- upTable[order(-abs(upTable$logFC)),]
+rn_up = upTable$gene
+
+write.csv(rn_up, up_genes_file_name, row.names = FALSE)
+write.csv(upTable, up_table_file_name, row.names = FALSE)
+
+down <- which(results[,1] == -1)
+downGenes <- fit2[down, ]
+downTable <- topTable(downGenes[,1], number = nrow(fit2), sort.by = "logFC") 
+downTable$gene <- rownames(downTable)
+downTable <- downTable[order(-abs(downTable$logFC)),]
+rn_down = downTable$gene
+
+write.csv(rn_down, down_genes_file_name, row.names = FALSE)
+write.csv(downTable, down_table_file_name, row.names = FALSE)
+
+dge_genes <- c(rn_up,rn_down)
+write.csv(dge_genes, deg_genes_file_name, row.names = FALSE)
+
+exprs_dge<-exprs_table[,which(colnames(exprs_table) %in% dge_genes)]
+write.csv(exprs_dge, deg_exprs_file_name)
+
+# ------------------------------------------------------------------------------
+
+message("Completed.")
+
+
+
+
+
+
+
+
+
+
+
+
+
